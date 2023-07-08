5692ed5ebdc57a5d041fec91d3fb329288e816be
John Polacek <johnpolacek@gmail.com>
2023-07-08 10:40:56 -0400

    style news articles

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -4,14 +4,16 @@ import { NewsArticle } from '../types'
 export default function Main({articles}: {articles: NewsArticle[]}) {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Not Yet News</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-16 max-w-[960px] mx-auto">
+      <h1 className="text-6xl pb-16 border-b">Not Yet News</h1>
       <div>
-        {articles.map((article, i) => (
-          <div key={`article-${i}`}>
-            <h3>{article.title}</h3>
-            <p>{article.abstract}</p>
-            <Image alt="" src={article.imageUrl} width={256} height={256} />
+        {articles.filter((article) => (article.title && article.abstract)).map((article, i) => (
+          <div className="flex gap-8 pb-12" key={`article-${i}`}>
+            <Image className='w-40 h-40' alt="" src={article.imageUrl || `/placeholder${Math.round(Math.random() * 6)}.png`} width={180} height={180} />
+            <div>
+              <h3 className="text-xl font-semibold pb-2 ">{article.title}</h3>
+              <p>{article.abstract}</p>
+            </div>
           </div>
         ))}
       </div>
