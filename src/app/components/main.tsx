8eef7dc99e326f28728092ffdca16fa4689f840d
John Polacek <johnpolacek@gmail.com>
2023-07-08 19:26:13 -0500

    responsive layout

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -13,20 +13,20 @@ export default function Main({articles}: {articles: NewsArticle[]}) {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16 max-w-[1400px] mx-auto">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 xl:p-16 max-w-[1400px] mx-auto">
       <div className="pb-8 border-[#aaa] border-double border-b-4 w-full mb-12 text-center">
         <h1 className="text-6xl">Not Yet News</h1>
         <h4>{new Date(new Date().setFullYear(new Date().getFullYear() + 100)).toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</h4>
       </div>
-      <div className="grid grid-cols-3 gap-8 divide-x">
+      <div className="grid lg:grid-cols-3 gap-8 divide-x">
         <div className="col-span-2 divide-y -mt-8">
           {topArticles.map((article, i) => (
-            <div className="flex gap-8 py-8" key={`article-${i}`}>
-              <div className="w-2/5">
+            <div className="flex flex-col flex-col-reverse md:flex-row gap-4 md:gap-8 py-8" key={`article-${i}`}>
+              <div className="w-full md:w-2/5">
                 <h3 className="text-xl font-semibold pb-2 ">{article.title}</h3>
                 <p>{article.abstract}</p>
               </div>
-              <div className="w-3/5 grow border">
+              <div className="w-full md:w-3/5 grow border">
                 <Image className='w-full h-auto' alt="" src={article.imageUrl || `/placeholder${Math.round(Math.random() * 6)}.png`} width={180} height={180} />
               </div>
             </div>
