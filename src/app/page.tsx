3f2272658bce7ec1ecedd46f4d86f43698233299
John Polacek <johnpolacek@gmail.com>
2023-07-09 20:28:03 -0500

    add cache revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image'
 import Main from './components/main'
 import { NewsArticle } from './types'
 
 async function getNewsArticles() {
-  const res = await fetch('https://notyetnews.s3.us-east-1.amazonaws.com/notyetnews-2023-07-09.json')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  const res = await fetch('https://notyetnews.s3.us-east-1.amazonaws.com/notyetnews-2023-07-09.json', { next: { revalidate: 60 } })
 
-  // Recommendation: handle errors
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
