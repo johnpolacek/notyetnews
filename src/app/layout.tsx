9a5c1648092655a36f6eb2d708c62eccde152563
John Polacek <johnpolacek@gmail.com>
2023-08-13 10:50:57 -0500

    add metadata for sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,27 @@
+import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Not Yet News',
-  description: 'News from the future',
+  description: 'AI-generated news from the future',
+  openGraph: {
+    title: 'Not Yet News',
+    description: 'AI-generated news from the future',
+    url: 'https://notyet.news',
+    siteName: 'Not Yet News',
+    images: [
+      {
+        url: 'https://notyet.news/screenshot.jpg',
+        width: 1191,
+        height: 708,
+        alt: 'Not Yet News, Speculative Satire from the Future',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
+
 
 export default function RootLayout({
   children,
